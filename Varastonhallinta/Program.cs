33080179c0e45550c834315f434a39b5c9b2b42a
NalleKing <mrpepu181@gmail.com>
2023-08-23 10:03:35 +0300

    Projektin perustaa

---
+++ Varastonhallinta/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Tietokantaharjoitus4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 - Lisää uusi tuote");
+            Console.WriteLine("2 - Poista tuote");
+            Console.WriteLine("3 - Tulosta eri tuotteiden määrät");
+            Console.WriteLine("4 - Tulosta kaikki tuotteet");
+            Console.WriteLine("5 - Muokkaa tuotenimeä");
+            Console.WriteLine("0 - Lopeta sovellus");
+
+
+        }
+    }
+}
